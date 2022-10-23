03a5d55c66065d993245bb167fd892210601e609
FilipchenkoDima <blewdinor@gmail.com>
2022-10-23 19:33:02 +0300

    Done 03

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,13 +1,42 @@
 var throttle = require('lodash.throttle');
 
-const feedbackFormState = { };
+const feedbackFormState = {};
+const LOCALSTORAGE_KEY = "feedback-form-state";
 
 const feedbackFormEl = document.querySelector('.feedback-form');
+const emailForm = document.querySelector('input[name="email"]');
+const massageForm = document.querySelector('textarea[name="message"]');
 
-feedbackFormEl.addEventListener('input', inputText);
+populateInput();
+
+feedbackFormEl.addEventListener('input', throttle(inputText, 1000));
+feedbackFormEl.addEventListener('submit', sendMessage)
 
 function inputText(evt) {
-    console.log(evt.target);
+    feedbackFormState[evt.target.name] = evt.target.value;
+    localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(feedbackFormState));
 };
 
-console.log('test');
+function populateInput() {
+    const savedInput = localStorage.getItem(LOCALSTORAGE_KEY);
+
+    if (savedInput) {
+        let parsedInput = JSON.parse(savedInput);
+
+        emailForm.value = parsedInput.email;
+        massageForm.textContent = parsedInput.message;
+    };
+};
+
+function sendMessage(evt) {
+    evt.preventDefault();
+
+    const user = {
+        email: evt.target.email.value,
+        message: evt.target.message.value
+    };
+    console.log(user);
+    
+    localStorage.removeItem(LOCALSTORAGE_KEY);
+    feedbackFormEl.reset();
+};
